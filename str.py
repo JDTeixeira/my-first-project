6d178c2b808add3ba248ec076cb0fcc21f2cb1a4
JDTeixeira <joao.daniel9@hotmail.com>
2022-07-26 20:08:50 +0100

    first part of class to calculate tire dimension from string

--- str.py
+++ str.py
@@ -1,2 +1,10 @@
-print("111")
-print('spam eggs')
+class TireDimension:
+    def __init__(self, TireDescription):
+        self.TireDescription=TireDescription
+        self.length=len(TireDescription)
+    def fct(self):
+        print(self.length) 
+
+p1= TireDimension("360/75 R 28")
+p1.fct()
+
